c66449a3bab3cbd7f2bb226ca363898a3564a12a
ERINDRAREYNALDI <79778685+ERINDRAREYNALDI@users.noreply.github.com>
2021-05-17 12:44:16 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php 
+$host       = "localhost";
+$user       = "root";
+$password   = "";
+$database   = "tugas10"; //nama database
+$koneksi    = mysqli_connect($host, $user, $password, $database);
+?>
